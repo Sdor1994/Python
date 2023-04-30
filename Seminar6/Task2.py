792dc831563d8c6ac40a07c42772d54107ebd538
Sdor1994 <122234785+Sdor1994@users.noreply.github.com>
2023-04-30 19:22:25 +0300

    Add files via upload

---
+++ Seminar6/Task2.py
@@ -0,0 +1,13 @@
+def find_indexes(arr, min_val, max_val):
+    indexes = []
+    for i in range(len(arr)):
+        if min_val <= arr[i] <= max_val:
+            indexes.append(i)
+    return indexes
+
+# пример использования
+arr = [1, 3, 5, 7, 9, 11, 13, 15, 17]
+min_val = 5
+max_val = 15
+indexes = find_indexes(arr, min_val, max_val)
+print(f"Элементы массива {arr}, значения которых принадлежат диапазону от {min_val} до {max_val}, имеют индексы: {indexes}")
